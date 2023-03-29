1bcd297baf440f307c532958ffae1458ff8ce690
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-03-29 13:11:52 +0300

    Add project files.

---
+++ Bloger.Ul/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bloger.Ul.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
