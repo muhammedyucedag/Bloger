c6087d1c02bc7ac669d13f01efa453ced1fd6606
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-03-29 16:41:28 +0300

    IUserservice,UserManager,IUserDal oluşturuldu ve register controller için geneter constuructor kuruldu

---
+++ Bloger.DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bloger.Entity.Concrete;
+
+namespace Bloger.DataAccess.Abstract
+{
+    public interface IUserDal : IGenericDal<User>
+    {
+        User GetUserByUsername(string username);
+    }
+}
---
+++ Bloger.Ul/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using Bloger.Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloger.Ul.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public RegisterController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
