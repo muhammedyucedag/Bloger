c87214db808d707b4507a2cfc5b82745a19eb271
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-04-26 20:24:11 +0300

    26.04.2023 baseentity eklendi. admin controller için blogler getirildi ve ekleme işlemi tamamlandı

---
+++ Bloger.DataAccess/Migrations/20230426172255_create-baseentity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloger.DataAccess.Migrations
+{
+    public partial class createbaseentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Blog",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Blog",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Blog");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Blog");
+        }
+    }
+}
---
+++ Bloger.Ul/Areas/Admin/Models/AdminControllerDetailBlogViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Bloger.Entity.Concrete;
+
+namespace Bloger.Ul.Areas.Admin.Models
+{
+    public class AdminControllerDetailBlogViewModel
+    {
+        public List<Blog> Blogs { get; set; }
+    }
+}
