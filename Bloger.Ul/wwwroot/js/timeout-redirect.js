cd7587c65668a85a85a63fe4a6ae9d1e453d1e69
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-04-29 14:46:17 +0300

    timeout.js değiştirildi

--- Bloger.Ul/wwwroot/js/timeout-redirect.js
+++ Bloger.Ul/wwwroot/js/timeout-redirect.js
@@ -1,4 +1,27 @@
-﻿(function () {
+﻿//(function () {
+//    const idleDurationSecs = 60; // 5 dakika, oturum süresi (saniye cinsinden)
+//    const redirectUrl = '/Login/LogOff'; // Yönlendirilecek URL
+//    let idleTimeout; // Zamanlayıcı fonksiyonu
+
+//    // Zamanlayıcı fonksiyonunu başlatın
+//    const resetIdleTimeout = function () {
+//        // Mevcut zamanlayıcıyı sıfırlayın
+//        if (idleTimeout) clearTimeout(idleTimeout);
+
+//        // Yeni bir zamanlayıcı belirleyin
+//        idleTimeout = setTimeout(() => location.href = redirectUrl, idleDurationSecs * 1000);
+//    };
+
+//    // Sayfa yüklenirken zamanlayıcıyı başlatır
+//    resetIdleTimeout();
+
+//    // Zamanlayıcıyı sıfırlayacak olayları belirler
+//    ['click', 'touchstart', 'mousemove'].forEach(evt =>
+//        document.addEventListener(evt, resetIdleTimeout, false)
+//    );
+//})();
+
+(function () {
     const idleDurationSecs = 60; // 5 dakika, oturum süresi (saniye cinsinden)
     const redirectUrl = '/Login/LogOff'; // Yönlendirilecek URL
     let idleTimeout; // Zamanlayıcı fonksiyonu
@@ -8,15 +31,21 @@
         // Mevcut zamanlayıcıyı sıfırlayın
         if (idleTimeout) clearTimeout(idleTimeout);
 
-        // Yeni bir zamanlayıcı belirleyin
-        idleTimeout = setTimeout(() => location.href = redirectUrl, idleDurationSecs * 1000);
+        // Yeni bir zamanlayıcı belirleyin, kullanıcının giriş yapmış olması durumunda
+        const isAuthenticated = document.getElementById('isAuthenticated').getAttribute('data-is-authenticated') === 'true';
+        if (isAuthenticated) {
+            idleTimeout = setTimeout(() => location.href = redirectUrl, idleDurationSecs * 1000);
+        }
     };
 
-    // Sayfa yüklenirken zamanlayıcıyı başlatın
-    resetIdleTimeout();
-
-    // Zamanlayıcıyı sıfırlayacak olayları belirleyin
-    ['click', 'touchstart', 'mousemove'].forEach(evt =>
-        document.addEventListener(evt, resetIdleTimeout, false)
-    );
+    // Sayfa yüklenirken zamanlayıcıyı başlatır, kullanıcının giriş yapmış olması durumunda
+    const isAuthenticated = document.getElementById('isAuthenticated').getAttribute('data-is-authenticated') === 'true';
+    if (isAuthenticated) {
+        resetIdleTimeout();
+        // Zamanlayıcıyı sıfırlayacak olayları belirler
+        ['click', 'touchstart', 'mousemove'].forEach(evt =>
+            document.addEventListener(evt, resetIdleTimeout, false)
+        );
+    }
 })();
+
