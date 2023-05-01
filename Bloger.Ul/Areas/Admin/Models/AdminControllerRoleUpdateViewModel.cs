cdd4d6cdfe31a1fe1e3d244603d17b157b9bb909
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-05-01 13:12:43 +0300

    01.05.2023 Admin rol için update alanı gerçekleştiridi.

---
+++ Bloger.Ul/Areas/Admin/Models/AdminControllerRoleUpdateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Bloger.Ul.Areas.Admin.Models
+{
+    public class AdminControllerRoleUpdateViewModel
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
