aa5778b41ac08b3c697d7006db14a494163edc3d
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-05-01 13:02:59 +0300

    01.05.2023 rolcontroller eklendi listeleme ve ekleme methohladı tamamlandı

---
+++ Bloger.DataAccess/Migrations/20230501083705_role-update-coloumn.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloger.DataAccess.Migrations
+{
+    public partial class roleupdatecoloumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "AspNetRoles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetRoles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetRoles");
+        }
+    }
+}
